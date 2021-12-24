835c952338259fcf9878912a51a348bd51330538
Julis Laptop <JulianM.sehne@gmail.com>
2021-12-24 09:09:47 +0100

    Xpercent methode war komplett broek, ist jetzt gefixt hoffentlich

--- src/game/sehnes/SiggiGameObject.java
+++ src/game/sehnes/SiggiGameObject.java
@@ -1,11 +1,18 @@
 package game.sehnes;
 
 import game.ItemKey;
+import game.Player;
 
 public class SiggiGameObject extends SehnesGameObject{
 
     public SiggiGameObject() {
-        super("Siggi", new ItemKey(SehnesGameObject.values[3].toLowerCase()),3,
-                80, "und hättest du auch das Kennzeichen gewusst :P", "404");
+        super("Siggi", new ItemKey(SehnesGameObject.values[3].toLowerCase()){
+                    @Override
+                    public void useItem(Player player){
+                        player.setMaxHealth(player.getMaxHealth() + amount);
+                        amount = 0;
+                    }
+                },3,
+                70, "und hättest du auch das Kennzeichen gewusst :P", "404");
     }
 }
